77b5b7edd0e21ab3dc2dacfab6dd248310e9beba
Igor Lapin <raynor73@gmail.com>
2020-12-11 17:57:19 +0000

    Add Expressions and AST Printer

---
+++ src/main/kotlin/AstPrinter.kt
@@ -0,0 +1,54 @@
+import java.lang.StringBuilder
+
+/**
+ * @author igorlapin on 11/12/2020.
+ */
+
+fun main() {
+    val expression = Expression.Binary(
+            Expression.Unary(
+                    Token(TokenType.MINUS, "-", null, 1),
+                    Expression.Literal(123)
+            ),
+            Token(TokenType.STAR, "*", null, 1),
+            Expression.Grouping(Expression.Literal(45))
+    )
+
+    println(AstPrinter().print(expression))
+}
+
+class AstPrinter : Expression.Visitor<String> {
+
+    fun print(expression: Expression): String {
+        return expression.accept(this)
+    }
+
+    override fun visitBinaryExpression(expression: Expression.Binary): String {
+        return parenthesize(expression.operator.lexeme, expression.left, expression.right)
+    }
+
+    override fun visitGroupingExpression(expression: Expression.Grouping): String {
+        return parenthesize("group", expression.expression)
+    }
+
+    override fun visitLiteralExpression(expression: Expression.Literal): String {
+        return expression.value?.toString() ?: "null"
+    }
+
+    override fun visitUnaryExpression(expression: Expression.Unary): String {
+        return parenthesize(expression.operator.lexeme, expression.right)
+    }
+
+    private fun parenthesize(name: String, vararg expressions: Expression): String {
+        val sb = StringBuilder()
+
+        sb.append("(").append(name)
+        expressions.forEach {
+            sb.append(" ")
+            sb.append(it.accept(this))
+        }
+        sb.append(")")
+
+        return sb.toString()
+    }
+}
---
+++ src/main/kotlin/Expression.kt
@@ -0,0 +1,38 @@
+/**
+ * @author igorlapin on 11/12/2020.
+ */
+sealed class Expression {
+
+    class Binary(val left: Expression, val operator: Token, val right: Expression) : Expression() {
+
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitBinaryExpression(this)
+    }
+
+    class Grouping(val expression: Expression) : Expression() {
+
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitGroupingExpression(this)
+    }
+
+    class Literal(val value: Any?) : Expression() {
+
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitLiteralExpression(this)
+    }
+
+    class Unary(val operator: Token, val right: Expression) : Expression() {
+
+        override fun <R> accept(visitor: Visitor<R>): R = visitor.visitUnaryExpression(this)
+    }
+
+    abstract fun <R> accept(visitor: Visitor<R>): R
+
+    interface Visitor<R> {
+
+        fun visitBinaryExpression(expression: Binary): R
+
+        fun visitGroupingExpression(expression: Grouping): R
+
+        fun visitLiteralExpression(expression: Literal): R
+
+        fun visitUnaryExpression(expression: Unary): R
+    }
+}
